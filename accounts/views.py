13a8ec61b00cd4a42f31ee5496a30ef235458d8c
Pau Vilella <pau.vilella@fundcraft.lu>
2022-11-30 23:58:48 +0100

    start over again

--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
-
-
-def get_all_accounts(request):
-    content = {"success": True}
-    return JsonResponse(content)
